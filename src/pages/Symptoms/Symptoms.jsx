6fa48a2e409ed909e955a3131640d304366d4005
priyab99 <priya.bd.9911@gmail.com>
2023-08-20 23:29:08 +0600

    symptoms selection done

--- src/pages/Symptoms/Symptoms.jsx
+++ src/pages/Symptoms/Symptoms.jsx
@@ -1,11 +1,54 @@
-
+import Multiselect from 'multiselect-react-dropdown';
+import { useState } from 'react';
 
 const Symptoms = () => {
+    const [options] = useState([
+        'Anxiety', 'Depression', 'Phobias', 'Low Self-Esteem', 'Self Harm',
+        'Loss and Grief', 'Stress', 'Mood Swings', 'Insomnia',
+        'Body Image Issues', 'Panic Attacks'
+    ]);
+
+    const [selectedSymptoms, setSelectedSymptoms] = useState([]);
+
+    const handleSelect = (selectedList) => {
+        setSelectedSymptoms(selectedList);
+    };
+
+    const handleRemove = (selectedList) => {
+        setSelectedSymptoms(selectedList);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Do something with the selectedSymptoms, like submitting the form
+        console.log("Selected symptoms:", selectedSymptoms);
+    };
+
     return (
-        <div>
-            <div>
-            <h2 className="text-3xl  text-white text-center">Help Us To Find You A Therapist</h2>
-            </div>
+        <div className='pt-16'>
+            <h2 className='text-center text-3xl font-extrabold mt-10 mb-5'>Help us to find you a Therapist</h2>
+            <form className="row g-3" onSubmit={handleSubmit}>
+
+                <div className="col-md-5">
+                    <label className="form-label">Select Symptoms</label>
+
+                    <div className="text-dark">
+                        <Multiselect
+                            isObject={false}
+                            options={options}
+                            selectedValues={selectedSymptoms}
+                            onRemove={handleRemove}
+                            onSelect={handleSelect}
+                            showCheckbox
+                        />
+                    </div>
+                </div>
+
+                <div className="col-12">
+                    <button type="submit" className="btn btn-primary justify-center align-middle mt-64 mb-5">Submit</button>
+                </div>
+
+            </form>
         </div>
     );
 };
--- src/pages/shared/footer/Footer.jsx
+++ src/pages/shared/footer/Footer.jsx
@@ -1,9 +1,40 @@
 
+import logo from '../../../assets/images/logo2.png'
 
 const Footer = () => {
     return (
         <div>
-            
+            <footer className="footer p-10 bg-base-200 text-base-content">
+                <div>
+                    <img src={logo} alt="" className="w-20 " />
+                    <p>MindConnect.<br />Providing reliable tech since 2023</p>
+                    <a className="link link-hover">Being a full-time psychologist,i make<br />my patients feel special so
+                        they can<br />discuss their problems openly.My<br />goal is to help people fighting their<br />
+                        fears and life issues.</a>
+                </div>
+                <div>
+                    <span className="footer-title">Contact Details</span>
+                    <a className="link link-hover">Address</a>
+                    <a className="link link-hover">Phone Number</a>
+                    <a className="link link-hover">Email</a>
+                    <a className="link link-hover">Facebook</a>
+                    <a className="link link-hover">Time</a>
+                </div>
+                <div>
+                    <span className="footer-title">Our Programs</span>
+                    <a className="link link-hover">Dating and Relationship</a>
+                    <a className="link link-hover">Grief and Loss Counseling</a>
+                    <a className="link link-hover">Self Esteem Therapy</a>
+                    <a className="link link-hover">Kids and Family</a>
+                    <a className="link link-hover">Life and Future Planning</a>
+                    <a className="link link-hover">Old Age  Therapy</a>
+                </div>
+                <div>
+                    <span className="footer-title">Book Appoinment</span>
+                    <a className="link link-hover">let us chat(01816-149281)</a>
+                    <a className="link link-hover">Contact us now for a quote about<br /> consultation(Available 24/7)</a>
+                </div>
+            </footer>
         </div>
     );
 };
