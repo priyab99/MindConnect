aeb94006ead38a7ad28bee666293f43dadc011dd
Suraia-Tabassum <140569431+Suraia-Tabassum@users.noreply.github.com>
2023-09-22 20:40:48 +0600

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,48 +1,25 @@
-import './App.css'
-import React, {useState} from 'react';
-import Select from 'react-dropdown-select';
+import React from 'react';
+import './App.css';
+import TherapistCard from './TherapistCard';
+import therapistImage from './assets/therapist.jpg'; // Update the import path
+
+const therapists = [
+  {
+    photoUrl: therapistImage,
+    name: 'Tasniya Chowdhury',
+    address: 'Sugandha abasik, Chawkbazar',
+    phone: '01815-098765',
+  },
+  // Add more therapist objects if needed
+];
 
 function App() {
-  
-  const [value, setValue] = useState([])
-  const options = [
-    { id : "depression", name : 1},
-    { id : "ptsd", name : 2},
-    { id : "ocd", name : 3},
-    { id : "Anxiety", name : 4},
-    { id : "hallucination", name : 5},
-    { id : "mood swings", name : 6},
-    { id : "low self-esttem", name : 7},
-
-  ]
-  
-  const handleButtonClick = () => {
-    // You can navigate programmatically using the Link component
-  };
-
   return (
-    <>
-
-    <h1 className='text center'>Welcome to software engineering project</h1>
-     
-  <Select
-
-name="Select your symptomps" 
-options = {options}
-labelField="id"
-valueField="name"
-multi onChange={value => setValue(value)}
-color='green'
-dropdownPosition="bottom"
-searchable="true"
-  
-  >
-  
- </Select>
-  
- <button className="btn btn-outline btn-primary">Submit</button>  
- 
-    </>
+    <div className="flex flex-wrap justify-center gap-4">
+      {therapists.map((therapist, index) => (
+        <TherapistCard key={index} therapist={therapist} />
+      ))}
+    </div>
   );
 }
 
